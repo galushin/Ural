1c0886cbde590efe2930d2286e148f8a20f6ca58
galushin <galushin@gmail.com>
2014-01-24 11:38:58 +0800

    is_permutation - пропускать общий префикс

--- ural/algorithm/details/algo_base.hpp
+++ ural/algorithm/details/algo_base.hpp
@@ -865,7 +865,9 @@ namespace details
     template <class Forward1, class Forward2, class BinaryPredicate>
     bool is_permutation(Forward1 s1, Forward2 s2, BinaryPredicate pred)
     {
-        // @todo Пропустить общий префикс
+        std::tie(s1, s2) = ural::details::mismatch(std::move(s1), std::move(s2), pred);
+        s1.shrink_front();
+        s2.shrink_front();
 
         for(; !!s1; ++ s1)
         {
