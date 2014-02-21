ea9a47da26fccf3f90929d52c9e2b88096208103
galushin <galushin@gmail.com>
2014-02-21 15:28:38 +0800

    документация

--- ural/archetypes.hpp
+++ ural/archetypes.hpp
@@ -11,9 +11,16 @@ namespace ural
 {
 namespace archetypes
 {
+    /** @brief Архетип функционального объекта (остаётся неопределённым)
+    @tparam Signature сигнатура
+    */
     template <class Signature>
     class functor;
 
+    /** @brief Архетип функционального объекта (общее определение)
+    @tparam R типы аргументов
+    @tparam Args типы аргументов
+    */
     template <class R, class... Args>
     class functor<R(Args...)>
     {
@@ -31,6 +38,9 @@ namespace archetypes
         }
     };
 
+    /** @brief Архетип функционального объекта без возвращаемого значения
+    @tparam Args типы аргументов
+    */
     template <class... Args>
     class functor<void(Args...)>
     {
@@ -39,26 +49,43 @@ namespace archetypes
         {}
     };
 
+    /** @brief Архетип последовательности ввода
+    @tparam Value тип значения
+    */
     template <class Value>
     class input_sequence
      : public ural::sequence_base<input_sequence<Value>>
     {
     public:
+        /// @brief Тип значения
         typedef Value value_type;
+
+        /// @brief Тип ссылки
         typedef value_type & reference;
+
+        /// @brief Тип для представления расстояния
         typedef size_t distance_type;
+
+        /// @brief Тэг категории обхода
         typedef single_pass_traversal_tag traversal_tag;
 
+        /** @brief Проверка исчерпания последовательности
+        @return @b true
+        */
         bool operator!() const
         {
             return true;
         }
 
+        /** @brief Первый элемент последовательности
+        @return Ссылка на первый элемент последовательности
+        */
         reference front() const
         {
             return this->value();
         }
 
+        /// @brief Переход к следующему элементу
         void pop_front()
         {}
 
--- ural/defs.hpp
+++ ural/defs.hpp
@@ -14,17 +14,31 @@ namespace ural
     struct use_default{};
     struct auto_tag{};
 
+    /** @brief Класс, содержащий @b typedef-объявление типа
+    @tparam T тип
+    */
     template <class T>
     struct declare_type
     {
+        /// @brief Объявляемый тип
         typedef T type;
     };
 
+    /** Если @c T совпадает с @c use_default, то результат @c --- Default,
+    иначе --- @c T.
+    @brief Класс-характеристика для определения значения шаблонного
+    параметра
+    @tparam T тип
+    @tparam Default тип, используемый по-умолчанию
+    */
     template <class T, class Default>
     struct default_helper
      : std::conditional<std::is_same<T, use_default>::value, Default, T>
     {};
 
+    /** @brief Оператор "равно" для пустых типов
+    @return true
+    */
     template <class T>
     constexpr typename std::enable_if<std::is_empty<T>::value, bool>::type
     operator==(T const &, T const &)
@@ -32,33 +46,58 @@ namespace ural
         return true;
     }
 
+    /** @brief Естественное определение оператора "не равно"
+    @param x левый операнд
+    @param y правый операнд
+    @return <tt> !(x == y) </tt>
+    */
     template <class T1, class T2>
     constexpr bool operator!=(T1 const & x, T2 const & y)
     {
         return !(x == y);
     }
 
+    /** @brief Естественное определение оператора "больше"
+    @param x левый операнд
+    @param y правый операнд
+    @return <tt> y < x </tt>
+    */
     template <class T1, class T2>
     constexpr bool operator>(T1 const & x, T2 const & y)
     {
         return y < x;
     }
 
+    /** @brief Естественное определение оператора "меньше либо равно"
+    @param x левый операнд
+    @param y правый операнд
+    @return <tt> !(x > y) </tt>
+    */
     template <class T1, class T2>
     constexpr bool operator<=(T1 const & x, T2 const & y)
     {
         return !(x > y);
     }
 
+    /** @brief Естественное определение оператора "больше либо равно"
+    @param x левый операнд
+    @param y правый операнд
+    @return <tt> !(x < y) </tt>
+    */
     template <class T1, class T2>
     constexpr bool operator>=(T1 const & x, T2 const & y)
     {
         return !(x < y);
     }
 
+    /** @brief Вспомогательный класс, которому можно присвоить значение
+    заданного типа
+    @tparam T тип присваемоего значения
+    */
     template <class T>
     struct value_consumer
     {
+        /// @brief Оператор присваивания
         void operator=(T const &)
         {}
     };
