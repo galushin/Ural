00907c484e3103393ed51a8c9414fdfd6459bd06
galushin <galushin@gmail.com>
2014-03-03 17:18:15 +0800

    Добавлены сведения о лицензии в файлы корневой папки

--- ural/random.hpp
+++ ural/random.hpp
@@ -1,6 +1,22 @@
 #ifndef Z_URAL_RANDOM_HPP_INCLUDED
 #define Z_URAL_RANDOM_HPP_INCLUDED
 
+/*  This file is part of Ural.
+
+    Ural is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ural is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ural.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /** @file ural/random.hpp
  @brief Средства генерации случайных чисел
  @todo Пересмотр и оптимизация средств ввода/вывода
