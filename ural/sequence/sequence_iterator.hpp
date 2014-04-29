80cc8490d0d04c17baa460c5283dfcdbe2b65603
galushin <galushin@gmail.com>
2014-04-29 09:49:41 +0800

    Документация sequence_iterator

--- ural/sequence/sequence_iterator.hpp
+++ ural/sequence/sequence_iterator.hpp
@@ -1,13 +1,43 @@
 #ifndef Z_URAL_SEQUENCE_SEQUENCE_ITERATOR_HPP_INCLUDED
 #define Z_URAL_SEQUENCE_SEQUENCE_ITERATOR_HPP_INCLUDED
+/*  This file is part of Ural.
+
+    Ural is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Ural is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Ural.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/** @file ural/sequence/sequence_iterator.hpp
+ @brief Итератор на базе последовательности. Основная цель --- интеграция с
+ циклом @c for для интервалов.
+*/
 
 #include <ural/optional.hpp>
 
 namespace ural
 {
+    /** @brief Итератор на базе последовательности. Основная цель ---
+    интеграция с циклом @c for для интервалов.
+    @param Sequence последовательность
+    */
     template <class Sequence>
     class sequence_iterator
     {
+        /** @brief Оператор "равно"
+        @param x левый операнд
+        @param y правый операнд
+        @return @b true, если последовательность @c x исчерпана, иначе
+        --- @b false.
+        */
         friend bool operator==(sequence_iterator const & x,
                                sequence_iterator const & y)
         {
@@ -17,24 +47,36 @@ namespace ural
 
     public:
         // Типы
+        /// @brief Тип ссылки
         typedef typename Sequence::reference reference;
 
         // Конструктор
+        /** @brief Конструктор по-умолчанию. Создаёт итератор конца
+        последовательности
+        */
         sequence_iterator()
          : impl_{nullopt}
         {}
 
+        /** @brief Создание начального итератора для последовательности
+        */
         sequence_iterator(Sequence s)
          : impl_{std::move(s)}
         {}
 
         // Итератор ввода
+        /** @brief Ссылка на текущий элемент
+        @return Ссылка на текущий элемент
+        */
         reference operator*() const
         {
             assert(!!impl_);
             return **impl_;
         }
 
+        /** @brief Переход к следующему элементу
+        @return <tt> *this </tt>
+        */
         sequence_iterator & operator++()
         {
             assert(!!impl_);
