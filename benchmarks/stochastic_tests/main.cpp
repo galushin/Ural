dbe77ef2fb80fe955030c2b7b8c0ebe36b6ee63a
galushin <galushin@gmail.com>
2016-12-15 21:46:31 +0700

    #43 Проверка отсутствия зависимости результата от исходной последовательности в алгоритме shuffle

--- benchmarks/stochastic_tests/main.cpp
+++ benchmarks/stochastic_tests/main.cpp
@@ -24,24 +24,29 @@ IntType factorial(IntType n)
     return result;
 }
 
-int main(int argc, char const * argv[])
+template <class Random_engine, class Container>
+map<Container, size_t>
+frequences_of_shuffle(Random_engine & rnd, Container const & s0, size_t iterations)
+{
+    std::map<std::string, size_t> counter;
+
+    for(int n = iterations; n > 0; -- n)
+    {
+        auto s = s0;
+        ural::shuffle(s, rnd);
+        counter[s] += 1;
+    }
+
+    return counter;
+}
+
+void check_shuffle_uniformity(size_t iterations)
 {
     typedef std::mt19937 Random_engine;
     Random_engine rnd(std::time(0));
 
-    std::map<std::string, size_t> counter;
-
     std::string const s0("abcd");
-    auto s = s0;
-
-    auto iterations = 10'000;
-    if (argc > 1) iterations = std::atoi(argv[1]);
-
-    for(int n = iterations; n > 0; -- n)
-    {
-        ural::shuffle(s, rnd);
-        counter[s] += 1;
-    }
+    auto const counter = frequences_of_shuffle(rnd, s0, iterations);
 
     assert(counter.size() == factorial(s0.size()));
 
@@ -65,6 +70,56 @@ int main(int argc, char const * argv[])
     cout << p << endl;
 
     assert(p < 1e-6);
+}
+
+void check_shuffle_independency(size_t iterations)
+{
+    typedef std::mt19937 Random_engine;
+    Random_engine rnd(std::time(0));
+
+    std::string const s1("abcd");
+    std::string const s2
+        = [s1, &rnd]() { auto s = s1; std::shuffle(s.begin(), s.end(), rnd); return s; }();
+
+    assert(ural::is_permutation(s1, s2));
+
+    auto const counter_1 = frequences_of_shuffle(rnd, s1, iterations);
+    auto const counter_2 = frequences_of_shuffle(rnd, s2, iterations);
+
+    assert(counter_1.size() == factorial(s1.size()));
+    assert(counter_2.size() == factorial(s1.size()));
+
+    // Вычисляем статистику хи-квадрат
+    double z = 0.0;
+
+    for(auto const & p1 : counter_1)
+    {
+        auto const f1 = double(p1.second);
+        auto const f2 = counter_2.at(p1.first);
+
+        z += ural::square(double(f1) / iterations - double(f2) / iterations) / (f1 + f2);
+    }
+
+    z *= counter_1.size();
+    z *= counter_2.size();
+
+    // Выводим вероятность такого значения
+    boost::math::chi_squared_distribution<double> chi_2(counter_1.size() - 1);
+
+    auto const p = cdf(chi_2, z);
+
+    cout << p << endl;
+
+    assert(p < 1e-6);
+}
+
+int main(int argc, char const * argv[])
+{
+    auto iterations = 1'000;
+    if (argc > 1) iterations = std::atoi(argv[1]);
+
+    check_shuffle_uniformity(iterations);
+    check_shuffle_independency(iterations);
 
     return 0;
 }
